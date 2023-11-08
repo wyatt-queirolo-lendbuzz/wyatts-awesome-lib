cf1e0328624a2092c6fee96a2b7ac42bb4c98c45
Wyatt Queirolo <wyatt.queirolo@lendbuzz.com>
2023-11-08 12:26:21 -0500

    fixed teh bug

--- src/wyatts_awesome_lib/main.py
+++ src/wyatts_awesome_lib/main.py
@@ -2,4 +2,4 @@ def anothermainfunction():
     print("hello world")
     print("this is another feature!")
     print("I fixed a bug")
-    print("heres a feature witha a bug")
+    print("heres a feature with a bug")
