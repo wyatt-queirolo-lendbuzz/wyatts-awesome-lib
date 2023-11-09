6045ef2479cf938d367648c89385067d5459fe2a
wyatt-queirolo-lendbuzz <148131542+wyatt-queirolo-lendbuzz@users.noreply.github.com>
2023-11-09 13:54:55 -0500

    feat: added a print statement (#8)
    description of the feature

--- src/wyatts_awesome_lib/main.py
+++ src/wyatts_awesome_lib/main.py
@@ -2,4 +2,5 @@ def anothermainfunction():
     print("hello world")
     print("this is another feature!")
     print("I fixed a bug")
+    print("heres my feature")
     print("heres a feature with a bug")
